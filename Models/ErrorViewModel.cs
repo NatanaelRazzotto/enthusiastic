ee43a69435b56671483f5b6eaa580af656747f9e
Natanael <Natanael@DESKTOP-C0F71C1>
2023-02-02 19:02:11 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace enthusiastic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
