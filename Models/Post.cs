a2fa97f730417d4e9afe3c7eef4d4dac6bb598bf
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2023-02-04 23:24:08 -0300

    corrections view feed

--- Models/Post.cs
+++ Models/Post.cs
@@ -8,7 +8,7 @@ namespace enthusiastic.Models
         public string? Title { get; set; }
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
-        public string Autor { get; set; }
+        public string? Autor { get; set; }
         public string Content { get; set; }
   
     }
